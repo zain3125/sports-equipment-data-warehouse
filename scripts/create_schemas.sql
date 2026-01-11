4f83f5e91c5705c9daca393673b8d97c9253340d
zain <zeine712342@gmail.com>
2026-01-11 18:21:04 +0200

    Create schemas

---
+++ scripts/create_schemas.sql
@@ -0,0 +1,22 @@
+/*
+=============================================================
+Create Schemas
+=============================================================
+Script Purpose:
+    This script creates a new schemas after checking if it already exists. 
+    If the schemas exists, it is dropped and recreated. Additionally
+	
+WARNING:
+    Running this script will drop the entire schema if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
+DROP SCHEMA IF EXISTS bronz CASCADE;
+CREATE SCHEMA bronz;
+
+DROP SCHEMA IF EXISTS silver CASCADE;
+CREATE SCHEMA silver;
+
+DROP SCHEMA IF EXISTS gold CASCADE;
+CREATE SCHEMA gold;
