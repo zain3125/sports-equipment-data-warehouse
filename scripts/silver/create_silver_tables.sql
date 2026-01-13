e9ec8d2e6421c298c75f1b6cd8b60f3f3ccb606a
zain3125 <zeine712342@gmail.com>
2026-01-13 19:39:11 +0200

    Create silver tables schema

---
+++ scripts/silver/create_silver_tables.sql
@@ -0,0 +1,77 @@
+/*
+=================================
+Create tables for the silver layer
+=================================
+Script Purpose:
+    This script creates a new Tables -created dwh_create_ts For Debugging purposes-
+    after checking if it already exists. 
+    If the Table exists, it is dropped and recreated. Additionally
+	
+WARNING:
+    Running this script will drop the entire Tables if it exists. 
+    Proceed with caution and ensure you have proper backups before running this script.
+
+*/
+-- CRM tables schema
+DROP TABLE IF EXISTS silver.crm_cust_info;
+CREATE TABLE silver.crm_cust_info(
+	cst_id INT,
+	cst_key VARCHAR(20),
+	cst_firstname VARCHAR(50),
+	cst_lastname VARCHAR(50),
+	cst_marital_status VARCHAR(10),
+	cst_gndr VARCHAR(10),
+	cst_create_date DATE,
+    dwh_create_ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS silver.crm_prd_info;
+CREATE TABLE silver.crm_prd_info(
+	prd_id INT,
+	prd_key VARCHAR(50),
+	prd_nm VARCHAR(50),
+	prd_cost INT,
+	prd_line VARCHAR(10),
+	prd_start_dt DATE,
+	prd_end_dt DATE,
+    dwh_create_ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS silver.crm_sales_details;
+CREATE TABLE silver.crm_sales_details(
+	sls_ord_num VARCHAR(20),
+	sls_prd_key VARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT,
+    dwh_create_ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- ERP tables schema
+DROP TABLE IF EXISTS silver.erp_CUST_AZ12;
+CREATE TABLE silver.erp_CUST_AZ12(
+    CID VARCHAR(20),
+    BDATE DATE,
+    GEN VARCHAR(10),
+    dwh_create_ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS silver.erp_LOC_A101;
+CREATE TABLE silver.erp_LOC_A101(
+    CID VARCHAR(20),
+    CNTRY VARCHAR(50),
+    dwh_create_ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS silver.erp_PX_CAT_G1V2;
+CREATE TABLE silver.erp_PX_CAT_G1V2(
+    ID VARCHAR(20),
+    CAT VARCHAR(20),
+    SUBCAT VARCHAR(20),
+    MAINTENANCE VARCHAR(20),
+    dwh_create_ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
