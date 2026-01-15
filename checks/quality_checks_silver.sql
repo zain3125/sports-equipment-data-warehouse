820156b8353cda066e2f86de4bc401072eb5d8e0
zain <zeine712342@gmail.com>
2026-01-15 16:48:06 +0200

    More checks for crm_prd_info

--- checks/quality_checks_silver.sql
+++ checks/quality_checks_silver.sql
@@ -14,12 +14,12 @@ select cst_firstname, cst_lastname from silver.crm_cust_info
 where cst_firstname != TRIM(cst_firstname)
 or cst_lastname != TRIM(cst_lastname);
 
--- Check values of marital_status
+-- Data Standardization & Consistency
 select cst_marital_status
 FROM silver.crm_cust_info
 GROUP BY cst_marital_status;
 
--- Check values of gndr
+-- Data Standardization & Consistency
 select cst_gndr
 FROM silver.crm_cust_info
 GROUP BY cst_gndr;
@@ -34,6 +34,23 @@ Checking crm_prd_info
 select prd_id, count(*) from silver.crm_prd_info
 GROUP BY prd_id
 HAVING count(*) > 1 OR prd_id IS NULL;
+
+-- Check for Unwanted Spaces
+SELECT 
+    prd_nm 
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm);
+
+-- Check for NULLs or Negative Values in Cost
+SELECT 
+    prd_cost 
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL;
+
+-- Data Standardization & Consistency
+SELECT DISTINCT 
+    prd_line 
+FROM silver.crm_prd_info;
 
 -- Check start date is before end date
 select * from silver.crm_prd_info
