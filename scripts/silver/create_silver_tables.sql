4c04b7f31c2bd1de622b34f08252d9b1ab111ea3
zain <zeine712342@gmail.com>
2026-01-15 02:14:52 +0200

    Create new column cat_id

--- scripts/silver/create_silver_tables.sql
+++ scripts/silver/create_silver_tables.sql
@@ -28,6 +28,7 @@ CREATE TABLE silver.crm_cust_info(
 DROP TABLE IF EXISTS silver.crm_prd_info;
 CREATE TABLE silver.crm_prd_info(
 	prd_id INT,
+	cat_id VARCHAR(50),
 	prd_key VARCHAR(50),
 	prd_nm VARCHAR(50),
 	prd_cost INT,
