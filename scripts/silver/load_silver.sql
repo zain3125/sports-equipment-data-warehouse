5f481ef99e2028b40c2e3093a4ed713af48be5d7
zain <zeine712342@gmail.com>
2026-01-15 16:06:54 +0200

    Insert data to crm_prd_info silver layer

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -27,3 +27,30 @@ FROM
         PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag
     FROM bronz.crm_cust_info WHERE cst_id IS NOT NULL) t
 where flag = 1; 
+
+TRUNCATE TABLE silver.crm_prd_info;
+INSERT INTO silver.crm_prd_info(
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt)
+SELECT
+    prd_id,
+    REPLACE(substr(prd_key, 1, 5), '-', '_') AS cat_id,
+    REPLACE(substr(prd_key, 7), '-', '_') AS prd_key,
+    prd_nm,
+    COALESCE(prd_cost, 0) AS prd_cost,
+    CASE 
+        WHEN UPPER(TRIM(prd_line)) = 'S' THEN  'Other Sales'
+        WHEN UPPER(TRIM(prd_line)) = 'T' THEN  'Touring'
+        WHEN UPPER(TRIM(prd_line)) = 'M' THEN  'Mountain'
+        WHEN UPPER(TRIM(prd_line)) = 'R' THEN  'Road'
+        ELSE  'N/A'
+    END AS prd_line,
+    prd_start_dt,
+    LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt) -1 AS prd_end_dt
+FROM bronz.crm_prd_info;
