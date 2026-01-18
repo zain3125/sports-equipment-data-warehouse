2ed1d2b10ddc3064144d75fce86a4cfaed308bc0
zain <zeine712342@gmail.com>
2026-01-18 14:56:47 +0200

    Check data of crm_sales_details silver layer

--- checks/quality_checks_silver.sql
+++ checks/quality_checks_silver.sql
@@ -5,22 +5,22 @@ Checking crm_cust_info
 */
 
 -- Check id duplicates and NULL id's
-select cst_id, count(*) from silver.crm_cust_info
+SELECT cst_id, count(*) FROM silver.crm_cust_info
 GROUP BY cst_id
 HAVING count(*) > 1 OR cst_id IS NULL;
 
 -- Check spaces before OR after name
-select cst_firstname, cst_lastname from silver.crm_cust_info
-where cst_firstname != TRIM(cst_firstname)
-or cst_lastname != TRIM(cst_lastname);
+SELECT cst_firstname, cst_lastname FROM silver.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
+OR cst_lastname != TRIM(cst_lastname);
 
 -- Data Standardization & Consistency
-select cst_marital_status
+SELECT cst_marital_status
 FROM silver.crm_cust_info
 GROUP BY cst_marital_status;
 
 -- Data Standardization & Consistency
-select cst_gndr
+SELECT cst_gndr
 FROM silver.crm_cust_info
 GROUP BY cst_gndr;
 
@@ -31,7 +31,7 @@ Checking crm_prd_info
 */
 
 -- Check prd_id duplicates and NULL prd_id's
-select prd_id, count(*) from silver.crm_prd_info
+SELECT prd_id, count(*) FROM silver.crm_prd_info
 GROUP BY prd_id
 HAVING count(*) > 1 OR prd_id IS NULL;
 
@@ -53,7 +53,7 @@ SELECT DISTINCT
 FROM silver.crm_prd_info;
 
 -- Check start date is before end date
-select * from silver.crm_prd_info
+SELECT * FROM silver.crm_prd_info
 WHERE prd_end_dt < prd_start_dt;
 
 /*
@@ -62,22 +62,24 @@ Checking crm_sales_details
 ============================
 */
 -- Check spaces before OR after prd_key
-select sls_prd_key from silver.crm_sales_details
-where sls_prd_key != TRIM(sls_prd_key);
+SELECT sls_prd_key FROM silver.crm_sales_details
+WHERE sls_prd_key != TRIM(sls_prd_key);
 
--- Check order of Dates
-SELECT * FROM bronz.crm_sales_details
-where sls_order_dt > sls_ship_dt
-or sls_ship_dt > sls_due_dt;
+-- Check logic order of Dates
+SELECT * FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt
+OR sls_ship_dt > sls_due_dt;
 
--- check price calc
-SELECT * FROM bronz.crm_sales_details
-where ABS(sls_price) != ABS(sls_sales) * sls_quantity;
+-- Check for NULLs or Negative Values
+SELECT * FROM silver.crm_sales_details
+WHERE
+sls_sales <= 0 OR sls_sales IS null
+OR sls_quantity <= 0 OR sls_quantity IS null
+OR sls_price <= 0 OR sls_price IS null;
 
-SELECT * FROM bronz.crm_sales_details
-where
-sls_sales <= 0 or sls_sales) IS null;
+-- check price calculation
+SELECT * FROM silver.crm_sales_details
+WHERE sls_sales != sls_price * sls_quantity;
 
-SELECT * FROM bronz.crm_sales_details
-where
-sls_quantity !=1;
+SELECT * FROM silver.crm_sales_details
+WHERE sls_sales != sls_price * sls_quantity;
