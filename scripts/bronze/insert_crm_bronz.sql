4f2c50d473ad55930d44ed6c02fa57978035b0f7
zain <zeine712342@gmail.com>
2026-01-22 15:00:02 +0200

    Make stored procedure for bronze layer

--- scripts/bronze/insert_crm_bronz.sql
+++ scripts/bronze/insert_crm_bronz.sql
@@ -1,41 +1,78 @@
 /*
 =================================
-Insert CRM data to tables
+Insert CRM data to bronze layer
 =================================
 Script Purpose:
     This script insert CRM data after checking if it already exists. 
-    If the data exists, it is dropped and recreated. Additionally
+    If the data exists, it is dropped and re-inserted. Additionally
 	
 WARNING:
     Running this script will drop the entire CRM data if it exists. 
     Proceed with caution and ensure you have proper backups before running this script.
 
 */
+CREATE OR REPLACE PROCEDURE bronze.load_bronze_layer()
+LANGUAGE PLPGSQL
+AS $$
+BEGIN
+    RAISE NOTICE '---------------------------------------------';
+    RAISE NOTICE 'Starting CRM data load into Bronze layer';
+    RAISE NOTICE '---------------------------------------------';
 
-TRUNCATE TABLE bronze.crm_cust_info;
-COPY bronze.crm_cust_info
-FROM '/mnt/ssd/sports-equipment-data-warehouse/data/source_crm_csv/cust_info.csv'
-WITH (
-    FORMAT csv,
-    HEADER true,
-    DELIMITER ','
-);
+    -- =============================
+    -- Load crm_cust_info
+    -- =============================
+    RAISE NOTICE 'Truncating table: bronze.crm_cust_info';
+    TRUNCATE TABLE bronze.crm_cust_info;
 
-TRUNCATE TABLE bronze.crm_prd_info;
-COPY bronze.crm_prd_info
-FROM '/mnt/ssd/sports-equipment-data-warehouse/data/source_crm_csv/prd_info.csv'
-WITH (
-    FORMAT csv,
-    HEADER true,
-    DELIMITER ','
-);
+    RAISE NOTICE 'Loading data into: bronze.crm_cust_info';
+    COPY bronze.crm_cust_info
+    FROM '/mnt/ssd/sports-equipment-data-warehouse/data/source_crm_csv/cust_info.csv'
+    WITH (
+        FORMAT csv,
+        HEADER true,
+        DELIMITER ','
+    );
 
-TRUNCATE TABLE bronze.crm_sales_details;
-COPY bronze.crm_sales_details
-FROM '/mnt/ssd/sports-equipment-data-warehouse/data/source_crm_csv/sales_details.csv'
-WITH (
-    FORMAT csv,
-    HEADER true,
-    DELIMITER ','
-);
-select * from bronze.crm_prd_info;
+    -- =============================
+    -- Load crm_prd_info
+    -- =============================
+    RAISE NOTICE 'Truncating table: bronze.crm_prd_info';
+    TRUNCATE TABLE bronze.crm_prd_info;
+
+    RAISE NOTICE 'Loading data into: bronze.crm_prd_info';
+    COPY bronze.crm_prd_info
+    FROM '/mnt/ssd/sports-equipment-data-warehouse/data/source_crm_csv/prd_info.csv'
+    WITH (
+        FORMAT csv,
+        HEADER true,
+        DELIMITER ','
+    );
+
+    -- =============================
+    -- Load crm_sales_details
+    -- =============================
+    RAISE NOTICE 'Truncating table: bronze.crm_sales_details';
+    TRUNCATE TABLE bronze.crm_sales_details;
+
+    RAISE NOTICE 'Loading data into: bronze.crm_sales_details';
+    COPY bronze.crm_sales_details
+    FROM '/mnt/ssd/sports-equipment-data-warehouse/data/source_crm_csv/sales_details.csv'
+    WITH (
+        FORMAT csv,
+        HEADER true,
+        DELIMITER ','
+    );
+
+    RAISE NOTICE '---------------------------------------------';
+    RAISE NOTICE 'CRM data loaded successfully into Bronze layer';
+    RAISE NOTICE '---------------------------------------------';
+
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE WARNING '---------------------------------------------';
+        RAISE WARNING 'Error occurred while loading CRM data into Bronze layer';
+        RAISE WARNING 'Error message: %', SQLERRM;
+        RAISE;
+END;
+$$;
--- scripts/bronze/schema_on_write_bronze.sql
+++ scripts/bronze/schema_on_write_bronze.sql
@@ -5,66 +5,94 @@ Create tables for the bronze layer
 Script Purpose:
     This script creates a new Tables after checking if it already exists. 
     If the Table exists, it is dropped and recreated. Additionally
-	
+
 WARNING:
     Running this script will drop the entire Tables if it exists. 
     Proceed with caution and ensure you have proper backups before running this script.
 
 */
--- CRM tables schema
-DROP TABLE IF EXISTS bronze.crm_cust_info;
-CREATE TABLE bronze.crm_cust_info(
-	cst_id INT,
-	cst_key VARCHAR(20),
-	cst_firstname VARCHAR(50),
-	cst_lastname VARCHAR(50),
-	cst_marital_status CHAR(1),
-	cst_gndr CHAR(1),
-	cst_create_date DATE
-);
+CREATE OR REPLACE PROCEDURE bronze.ddl_bronze_layer()
+LANGUAGE PLPGSQL
+AS $$
+BEGIN
+    RAISE NOTICE '------------------------------------------------';
+    RAISE NOTICE 'Starting Bronze Layer loading process';
+    RAISE NOTICE '------------------------------------------------';
 
-DROP TABLE IF EXISTS bronze.crm_prd_info;
-CREATE TABLE bronze.crm_prd_info(
-	prd_id INT,
-	prd_key VARCHAR(50),
-	prd_nm VARCHAR(50),
-	prd_cost INT,
-	prd_line CHAR(1),
-	prd_start_dt DATE,
-	prd_end_dt DATE
-);
+    -- ======================================================
+    -- CRM system tables
+    -- ======================================================
+    RAISE NOTICE '>> Creating CRM tables...';
 
-DROP TABLE IF EXISTS bronze.crm_sales_details;
-CREATE TABLE bronze.crm_sales_details(
-	sls_ord_num VARCHAR(20),
-	sls_prd_key VARCHAR(50),
-	sls_cust_id INT,
-	sls_order_dt INT,
-	sls_ship_dt INT,
-	sls_due_dt INT,
-	sls_sales INT,
-	sls_quantity INT,
-	sls_price INT
-);
+    DROP TABLE IF EXISTS bronze.crm_cust_info;
+    CREATE TABLE bronze.crm_cust_info(
+        cst_id INT,
+        cst_key VARCHAR(20),
+        cst_firstname VARCHAR(50),
+        cst_lastname VARCHAR(50),
+        cst_marital_status CHAR(1),
+        cst_gndr CHAR(1),
+        cst_create_date DATE
+    );
 
--- ERP tables schema
-DROP TABLE IF EXISTS bronze.erp_CUST_AZ12;
-CREATE TABLE bronze.erp_CUST_AZ12(
-	CID VARCHAR(20),
-	BDATE DATE,
-	GEN VARCHAR(10)
-);
+    DROP TABLE IF EXISTS bronze.crm_prd_info;
+    CREATE TABLE bronze.crm_prd_info(
+        prd_id INT,
+        prd_key VARCHAR(50),
+        prd_nm VARCHAR(50),
+        prd_cost INT,
+        prd_line CHAR(1),
+        prd_start_dt DATE,
+        prd_end_dt DATE
+    );
 
-DROP TABLE IF EXISTS bronze.erp_LOC_A101;
-CREATE TABLE bronze.erp_LOC_A101(
-	CID VARCHAR(20),
-	CNTRY VARCHAR(50)
-);
+    DROP TABLE IF EXISTS bronze.crm_sales_details;
+    CREATE TABLE bronze.crm_sales_details(
+        sls_ord_num VARCHAR(20),
+        sls_prd_key VARCHAR(50),
+        sls_cust_id INT,
+        sls_order_dt INT,
+        sls_ship_dt INT,
+        sls_due_dt INT,
+        sls_sales INT,
+        sls_quantity INT,
+        sls_price INT
+    );
 
-DROP TABLE IF EXISTS bronze.erp_PX_CAT_G1V2;
-CREATE TABLE bronze.erp_PX_CAT_G1V2(
-	ID VARCHAR(20),
-	CAT VARCHAR(20),
-	SUBCAT VARCHAR(20),
-	MAINTENANCE VARCHAR(20)
-);
+    -- ======================================================
+    -- ERP system tables
+    -- ======================================================
+    RAISE NOTICE '>> Creating ERP tables...';
+
+    DROP TABLE IF EXISTS bronze.erp_cust_az12;
+    CREATE TABLE bronze.erp_cust_az12(
+        cid VARCHAR(20),
+        bdate DATE,
+        gen VARCHAR(10)
+    );
+
+    DROP TABLE IF EXISTS bronze.erp_loc_a101;
+    CREATE TABLE bronze.erp_loc_a101(
+        cid VARCHAR(20),
+        cntry VARCHAR(50)
+    );
+
+    DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
+    CREATE TABLE bronze.erp_px_cat_g1v2(
+        id VARCHAR(20),
+        cat VARCHAR(20),
+        subcat VARCHAR(20),
+        maintenance VARCHAR(20)
+    );
+
+    RAISE NOTICE '------------------------------------------------';
+    RAISE NOTICE 'All tables have been created successfully!';
+    RAISE NOTICE '------------------------------------------------';
+
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE WARNING 'An error occurred while creating the tables';
+        RAISE WARNING 'Error message: %', SQLERRM;
+        RAISE;
+END;
+$$;
