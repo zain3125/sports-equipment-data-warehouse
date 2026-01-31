635684fb8f7791423acaa3e43dd7361a63713767
zain <zeine712342@gmail.com>
2026-01-31 09:11:38 +0200

    Making gold quality checks

--- checks/quality_checks_gold.sql
+++ checks/quality_checks_gold.sql
@@ -1,44 +1,17 @@
-SELECT prd_key, COUNT(*) FROM (
-    SELECT 
-        pn.prd_id,
-        pn.cat_id,
-        pn.prd_key,
-        pn.prd_nm,
-        pn.prd_cost,
-        pn.prd_start_dt,
-        pc.id,
-        pc.cat,
-        pc.subcat,
-        pc.maintenance
-    FROM silver.crm_prd_info pn
-    LEFT JOIN silver.erp_px_cat_g1v2 pc ON pn.cat_id = pc.id
-    WHERE prd_end_dt IS NULL --Filter historical data
-    ) t
-GROUP BY prd_key
+-- Check for duplicates of products Key in gold.dim_products
+SELECT product_number, COUNT(*)
+    FROM gold.dim_products
+GROUP BY product_number
 HAVING COUNT(*) > 1;
 
-
-
-SELECT cst_id, COUNT(*) FROM (
-    SELECT
-        ci.cst_id,
-        ci.cst_key,
-        ci.cst_firstname,
-        ci.cst_lastname,
-        ci.cst_marital_status,
-        ci.cst_gndr,
-        ci.cst_create_date,
-        ca.bdate,
-        ca.gen,
-        la.cntry
-    FROM silver.crm_cust_info ci
-    LEFT JOIN silver.erp_cust_az12 ca ON ci.cst_key = ca.cid
-    LEFT JOIN silver.erp_loc_a101 la ON ci.cst_key = la.cid
-    ) t
-GROUP BY cst_id
+-- Check for duplicates of customers Key in gold.dim_customers
+SELECT
+    customer_id, COUNT(*)
+FROM gold.dim_customers
+GROUP BY customer_id
 HAVING COUNT(*) > 1;
 
-
+-- Check data integration for gold.dim_customers
 SELECT DISTINCT
     ci.cst_gndr,
     ca.gen,
@@ -50,3 +23,10 @@ FROM silver.crm_cust_info ci
 LEFT JOIN silver.erp_cust_az12 ca ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 la ON ci.cst_key = la.cid
 ORDER BY 1, 2;
+
+-- Check the data model connectivity between facts and dimensions
+SELECT *
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p ON p.product_key = f.product_key
+LEFT JOIN gold.dim_customers c ON c.customer_key = f.customer_key
+WHERE p.product_key IS NULL OR c.customer_key IS NULL
