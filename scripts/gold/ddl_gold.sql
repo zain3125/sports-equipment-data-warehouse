700d27cc9c8b937518c44547c0d0a5c860dd5ffb
zain <zeine712342@gmail.com>
2026-01-31 09:10:43 +0200

    Make fact views queries

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,3 +1,4 @@
+CREATE OR REPLACE VIEW gold.dim_products AS
 SELECT
     ROW_NUMBER() over(ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
     pn.prd_id AS product_id,
@@ -12,9 +13,9 @@ SELECT
     pn.prd_start_dt AS start_date
 FROM silver.crm_prd_info pn
 LEFT JOIN silver.erp_px_cat_g1v2 pc ON pn.cat_id = pc.id
-WHERE prd_end_dt IS NULL --Filter historical data;
+WHERE prd_end_dt IS NULL; --Filter historical data
 
-
+CREATE OR REPLACE VIEW gold.dim_customers AS
 SELECT
     ROW_NUMBER() OVER(ORDER BY cst_id) AS customer_key,
     ci.cst_id AS customer_id,
@@ -33,3 +34,17 @@ FROM silver.crm_cust_info ci
 LEFT JOIN silver.erp_cust_az12 ca ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 la ON ci.cst_key = la.cid;
 
+CREATE OR REPLACE VIEW gold.fact_sales AS
+SELECT
+    sd.sls_ord_num AS order_number,
+    pr.product_key,
+    cu.customer_key,
+    sd.sls_order_dt AS order_date,
+    sd.sls_ship_dt AS shipping_date,
+    sd.sls_due_dt AS due_date,
+    sd.sls_sales AS sales_amount,
+    sd.sls_quantity AS quantity,
+    sd.sls_price AS price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu ON sd.sls_cust_id = cu.customer_id;
