9d5f818c77a14fe5004d420042c981dd4ffec3b5
zain <zeine712342@gmail.com>
2026-05-12 19:19:27 +0300

    Improve database setup script with logging and dynamic paths

--- scripts/init_DB.sh
+++ scripts/init_DB.sh
@@ -1,10 +1,45 @@
 #!/bin/bash
-# Variables
+
 DB_NAME="sports_DWH"
 DB_SUPERUSER="zain_super"
 DB_PASSWORD="password"
 
-# Create Superuser and DB
-psql -U postgres -c "CREATE USER $DB_SUPERUSER WITH PASSWORD '$DB_PASSWORD' SUPERUSER;"
+echo "Starting database setup..."
 
-psql -U postgres -c "CREATE DATABASE \"$DB_NAME\" OWNER $DB_SUPERUSER;"
+echo "Checking/Creating PostgreSQL user..."
+sudo -u postgres psql <<EOF
+DO \$\$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '$DB_SUPERUSER') THEN
+      CREATE USER $DB_SUPERUSER WITH PASSWORD '$DB_PASSWORD' SUPERUSER;
+   END IF;
+END
+\$\$;
+EOF
+
+echo "User check/creation done"
+
+echo "Checking/Creating database..."
+sudo -u postgres psql <<EOF
+SELECT 'CREATE DATABASE $DB_NAME'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$DB_NAME')\gexec
+EOF
+
+echo "Database ready"
+
+REPO_DIR="$(git rev-parse --show-toplevel)"
+
+echo "Copying CRM CSV files..."
+sudo mkdir -p /var/lib/postgresql/crm_data
+
+sudo cp -r "$REPO_DIR/data/source_crm_csv/"* \
+/var/lib/postgresql/crm_data/
+
+echo "Files copied successfully"
+
+sudo chown -R postgres:postgres /var/lib/postgresql/crm_data
+sudo chmod -R 755 /var/lib/postgresql/crm_data
+
+echo "Permissions fixed"
+
+echo "Setup completed successfully"
